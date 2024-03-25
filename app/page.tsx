55aa65d96595057925ab30b587f243ded95044f8
Bridget Walsh <bridget.walsh@mail.mcgill.ca>
2024-03-25 13:53:05 -0400

    more components

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { motion } from "framer-motion";
 import { TracingBeam } from "../components/ui/tracing-beam";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import { MacbookScroll } from "@/components/ui/macbook-scroll";
+import { Spotlight } from "@/components/ui/Spotlight";
+import { SignupFormDemo } from "@/components/ui/SignUpForm";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -30,6 +32,24 @@ export default function Home() {
       </motion.div>
       </AuroraBackground>
       <MacbookScroll></MacbookScroll>
+      <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight 
+      className="-top-40 left-0 md:left-60 md:-top-20"
+      fill="white" 
+      />
+       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Spotlight <br /> is the new trend.
+        </h1>
+        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+          Spotlight effect is a great way to draw attention to a specific part
+          of the page. Here, we are drawing the attention towards the text
+          section of the page. I don&apos;t know why but I&apos;m running out of
+          copy.
+        </p>
+      </div>
+      </div>
+  <SignupFormDemo></SignupFormDemo>
       </TracingBeam>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,8 +16,12 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
       animation: {
         aurora: "aurora 60s linear infinite",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
       boxShadow: {
         input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
@@ -29,6 +33,16 @@ module.exports = {
           },
           to: {
             backgroundPosition: "350% 50%, 350% 50%",
+          },
+          spotlight: {
+            "0%": {
+              opacity: 0,
+              transform: "translate(-72%, -62%) scale(0.5)",
+            },
+            "100%": {
+              opacity: 1,
+              transform: "translate(-50%,-40%) scale(1)",
+            },
           },
         },
       },
